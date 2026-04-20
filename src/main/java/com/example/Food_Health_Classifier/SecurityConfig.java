5d2a495b9c8576cc3f7ba39712b82fa90afcc07c
Parmeshwar wakde <149813448+Parmeshwar1111@users.noreply.github.com>
2026-04-20 13:47:27 +0530

    Update SecurityConfig to permit access to /api/users/**

--- src/main/java/com/example/Food_Health_Classifier/SecurityConfig.java
+++ src/main/java/com/example/Food_Health_Classifier/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
-                    "/api/**",
+                    "/api/users/**",
                     "/create-order",
                     "/verify-payment"
                 ).permitAll()
